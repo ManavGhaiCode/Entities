d61bacefa82d6970e05e128af7994290b5f01028
ManavGhaiCode <manavghai12october@gmail.com>
2023-12-30 16:01:04 +0530

    fix Shotgun

--- Scripts/Weapons/Shotgun.cs
+++ Scripts/Weapons/Shotgun.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
 
 public class Shotgun : Weapon {
-    public override void Shoot() {
-        for (int i = 0; i < 5; i++) {
-            int NewRotation = Random.Range(-40, 40);
-            firePoint.rotation = Quaternion.Euler(new Vector3 (0, 0, NewRotation));
+    public Transform[] FirePoints;
 
-            GameObject bullet = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+    public override void Shoot() {
+        int[] UsedIndexs = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            int FireIndex = Random.Range(0, FirePoints.Length);
+
+            for (int j = 0; j < UsedIndexs.Length; j++) {
+                while (UsedIndexs[j] == FireIndex) {
+                    FireIndex = Random.Range(0, FirePoints.Length);
+                }
+            }
+
+            GameObject bullet = Instantiate(BulletPrefab, FirePoints[FireIndex].position, FirePoints[FireIndex].rotation);
             Bullet bulletScript = bullet.GetComponent<Bullet>();
             bulletScript.Damage = bulletDamage;
             bulletScript.lifespan = bulletLifespan;
 
-            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(FirePoints[FireIndex].up * bulletSpeed, ForceMode2D.Impulse);
+            UsedIndexs[i] = FireIndex;
         }
     }
 }
